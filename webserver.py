de6de8b7cb462553f5376cbc432f92fd7e95f4ee
vagrant <vagrant@vagrant.vm>
2018-10-11 02:57:06 +0000

    feat: add post request

--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
 
 
 class WebServerHandler(BaseHTTPRequestHandler):
@@ -8,24 +9,58 @@ class WebServerHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            message = ""
-            message += "<html><body>Hello!</body></html>"
-            self.wfile.write(message.encode())
-            print(message)
+            output = ""
+            output += "<html><body>Hello!</body></html>"
+            output += '''<form method="POST" enctype="multipart/form-data"
+                        action="/hello"><h2>What would you like me to say?</h2>
+                        <input name="output" type="text" >
+                        <input type="submit" value="Submit"></form>'''
+            output += "</html></body>"
+            self.wfile.write(output.encode())
+            print(output)
             return
 
         if self.path.endswith("/hola"):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            message = ""
-            message += '''<html><body>&#161Hello! <a href = '/hello' >
-            Back to Hello</a></body></html>'''
-            self.wfile.write(message.encode())
-            print(message)
+            output = ""
+            output += '''<html><body>&#161Hello! <a href = '/hello' >
+                        Back to Hello</a>'''
+            output += '''<form method="POST" enctype="multipart/form-data"
+                        action="/hello"><h2>What would you like me to say?</h2>
+                        <input name="output" type="text" >
+                        <input type="submit" value="Submit"></form>'''
+            output += "</html></body>"
+            self.wfile.write(output.encode())
+            print(output)
             return
         else:
             self.send_error(404, 'File Not Found: %s' % self.path)
+
+    def do_POST(self):
+            self.send_response(301)
+            self.end_headers()
+
+            ctype, pdict = cgi.parse_header(self.headers.getheader(
+                'content-type'
+                )
+            )
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('output')
+
+            output = ""
+            output += "<html><body>"
+            output += " <h2> okey, how about this: </h2>"
+            output += "<h1> %s </h1>" % messagecontent[0]
+            output += '''<form method="POST" enctype="multipart/form-data"
+                        action="/hello"><h2>What would you like me to say?</h2>
+                        <input name="output" type="text" >
+                        <input type="submit" value="Submit"></form>'''
+            output += "</html></body>"
+            self.wfile.write(output.encode())
+            print(output)
 
 
 def main():
